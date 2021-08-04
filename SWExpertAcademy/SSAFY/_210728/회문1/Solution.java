ed622063f41a42ca38e6d529684eaf1797bb02f1
윤승일(Seung-Il Yoon) <liqurtJob@gmail.com>
2021-08-04 14:55:16 +0900

    리팩토링
    1. 가로, 세로 둘다 같은 메소드를 사용함
    2. 같은 메소드를 사용하기 위해 세로를 할때는 전치행렬화 해서 사용

--- SWExpertAcademy/SSAFY/_210728/회문1/Solution.java
+++ SWExpertAcademy/SSAFY/_210728/회문1/Solution.java
@@ -7,17 +7,15 @@ public class Solution {
     private static final int T = 10;
     private static final int BOARDSIZE = 8;
 
-    public static void main(String args[]) throws Exception
-    {
+    public static void main(String[] args) throws Exception {
         System.setIn(new FileInputStream("./src/_210728/dailyHW/회문1/input.txt"));
         Scanner sc = new Scanner(System.in);
 
-        for(int test_case = 1; test_case <= T; test_case++)
-        {
+        for(int test_case = 1; test_case <= T; test_case++) {
             int matchedHoeMoonLengthCount = 0;
-            int hoeMoonLength = sc.nextInt();
+            final int hoeMoonLength = sc.nextInt();
             char[][] board = new char[BOARDSIZE][BOARDSIZE];
-            char[] ca;
+
 
             for(int i = 0; i<BOARDSIZE ; i++){
                 String[] temp = sc.next().split("");
@@ -26,58 +24,57 @@ public class Solution {
                 }
             }
 
-            // 가로
-            for(int i=0 ; i<BOARDSIZE ; i++){
-                for(int start = 0; start<BOARDSIZE ; start++){
-                    for(int end = start+hoeMoonLength-1 ; end<BOARDSIZE ; end++){
-                        ca = new char[end-start+1];
-                        for(int k=0, S=start; k<ca.length ; k++){
-                            if(end<S){
-                                break;
-                            }
-                            ca[k] = board[i][S];
-                            S++;
-                        }
-                        if(HoeMoonLength(ca) == hoeMoonLength){
-                            matchedHoeMoonLengthCount++;
-                        }
-                    }
-                }
-            }
-            //지금 생각해보면, 세로 파트를 가로 파트를 복붙하는 것 보단 전치행렬을 사용하면 좀더 멋있을 것 같다.
-            // 세로
-            for(int i=0 ; i<BOARDSIZE ; i++){
-                for(int start = 0; start<BOARDSIZE ; start++){
-                    for(int end = start+hoeMoonLength-1 ; end<BOARDSIZE ; end++){
-                        ca = new char[end-start+1];
-                        for(int k=0, S=start; k<ca.length ; k++){
-                            if(end<S){
-                                break;
-                            }
-                            ca[k] = board[S][i];
-                            S++;
-                        }
-                        if(HoeMoonLength(ca) == hoeMoonLength){
-                            matchedHoeMoonLengthCount++;
-                        }
-                    }
-                }
-            }
+            matchedHoeMoonLengthCount = HoeMoon(matchedHoeMoonLengthCount, hoeMoonLength, board);
+            Transpose(board);
+            matchedHoeMoonLengthCount = HoeMoon(matchedHoeMoonLengthCount, hoeMoonLength, board);
 
             System.out.println("#"+test_case+" "+matchedHoeMoonLengthCount);
         }
     }
 
-    public static int HoeMoonLength(char[] ca) {
-        if (ca.length == 1) {
+    private static int HoeMoon(int matchedHoeMoonLengthCount, int hoeMoonLength, char[][] board) {
+        for(int i=0 ; i<BOARDSIZE ; i++){
+            for(int start = 0; start<BOARDSIZE ; start++){
+                for(int end = start+hoeMoonLength-1 ; end<BOARDSIZE ; end++){
+                    char[] charArray = new char[end-start+1];
+                    for(int k=0, S=start; k<charArray.length ; k++){
+                        if(end<S){
+                            break;
+                        }
+                        charArray[k] = board[i][S];
+                        S++;
+                    }
+                    if(HoeMoonLength(charArray) == hoeMoonLength){
+                        matchedHoeMoonLengthCount++;
+                    }
+                }
+            }
+        }
+        return matchedHoeMoonLengthCount;
+    }
+
+    public static int HoeMoonLength(char[] charArray) {
+        if (charArray.length == 1) {
             return 1;
         } else {
-            for (int start = 0, end = ca.length - 1; start < ca.length / 2; start++, end--) {
-                if (ca[start] != ca[end]) {
+            for (int start = 0, end = charArray.length - 1; start < charArray.length / 2; start++, end--) {
+                if (charArray[start] != charArray[end]) {
                     return 0;
                 }
             }
-            return ca.length;
+            return charArray.length;
+        }
+    }
+
+    private static void Transpose(char[][] board){
+        for(int i=0 ; i< board.length ; i++){
+            for(int j=0 ; j<board.length ; j++){
+                if(i<j){
+                    char temp = board[i][j];
+                    board[i][j] = board[j][i];
+                    board[j][i] = temp;
+                }
+            }
         }
     }
 }
