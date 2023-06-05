eb3c2857aab8f89f348d6e105040b744f2130d92
윤승일(Seung-Il Yoon) <liqurtJob@gmail.com>
2023-06-05 16:17:10 +0900

    BOJ2743

---
+++ BaekJoon/_2743/Main.kt
@@ -0,0 +1,14 @@
+package baekjoon._2743
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+class Main {
+}
+
+fun main(){
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val sb = StringBuilder()
+
+    print(sb.append(br.readLine().length))
+}
