bc16774960c0fdc26d10809e8bcb1529e4222dad
윤승일(Seung-Il Yoon) <liqurtJob@gmail.com>
2023-07-27 09:04:01 +0900

    BOJ18870 - 시간 초과2

--- BaekJoon/_18870/Main.kt
+++ BaekJoon/_18870/Main.kt
@@ -8,47 +8,61 @@ import java.util.*
 class Main {
 }
 
-fun main(){
+fun main(){ //아직도 시간초과
     // 입력
     val br = BufferedReader(InputStreamReader(System.`in`))
     val sb = StringBuilder()
+
     val n = br.readLine().toInt()
-    val originalXarr = Array<Int>(n){0}
+    val pointArr = Array(n){Point()}
     val st = StringTokenizer(br.readLine())
     while(st.hasMoreTokens()){
-        originalXarr[n-st.countTokens()] = st.nextToken().toInt()
+        val i = n-st.countTokens()
+        pointArr[i].apply {
+            originalIndex = i
+            originalValue = st.nextToken().toInt()
+        }
     }
-    val compressedXarr = Array<Int>(n){0}
 
-    // 시간초과된 첫번쨰 방법
-    val OVERNUMBER = 1000001
+    pointArr.sortBy { it.originalValue }
     var c = 0
-    while(originalXarr.min() != OVERNUMBER){ //O(n) = n, 밑에 꺼 포함하면 O(n) = n^2
-        val min = originalXarr.min()
-        val minIndexStack = Stack<Int>()
-        for (i in 0 until n){ // O(n) = n
-            if(originalXarr[i] == min){
-                minIndexStack.push(i)
-            }
+    var ci = 1
+
+    while(ci < n){ // O(n) = n 이 아닌가..?
+        if(pointArr[ci-1].originalValue != pointArr[ci].originalValue){
+            c++
         }
-        for (i in 0 until minIndexStack.size){ // O(n) = stack.size
-            val minIndex = minIndexStack.pop()
-            compressedXarr[minIndex] = c
-            originalXarr[minIndex] = OVERNUMBER
-        }
-        c++
+        pointArr[ci].compressIndex = ci
+        pointArr[ci].compressValue = c
+        ci++
     }
 
-    for (i in 0 until n){
-        sb.append(compressedXarr[i]).append(" ")
+    pointArr.sortBy { it.originalIndex }
+
+    for(i in 0 until n){
+        sb.append(pointArr[i].compressValue).append(" ")
     }
     sb.deleteCharAt(sb.length-1)
     print(sb)
 }
 
 data class Point(
-    val originalIndex : Int,
-    val originalValue : Int,
-    val compressIndex : Int,
-    val compressValue : Int
+    var originalIndex : Int = 0,
+    var originalValue : Int = 0,
+    var compressIndex : Int = 0,
+    var compressValue : Int = 0
 )
+
+/*
+1. 정렬
+2. c=0, ci=0
+3. 맨 왼쪽에 c 값 넣기
+4. ci++
+
+LOOP
+5. pA[ci] 값이 pA[ci-1]과 같은가?
+5-1. 같음 : pA[ci] = c
+5-2. 다름 : pA[ci] = ++c
+6. ci++
+7. n <= ci 이면, 루프 끝
+ */
