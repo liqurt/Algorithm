5326be44b44510f03b32d9fa4c3f714cef9b468e
윤승일(Seung-Il Yoon) <liqurtJob@gmail.com>
2023-07-28 09:34:03 +0900

    BOJ18870 - 시간 초과3 - 남의 자료 보고 배우기

--- BaekJoon/_18870/Main.kt
+++ BaekJoon/_18870/Main.kt
@@ -4,65 +4,45 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.lang.StringBuilder
 import java.util.*
+import kotlin.collections.HashMap
 
 class Main {
+    /*
+    순수하게 내 머리로 풀지 않았음, 남의 자료 참고함
+    출처 : https://girawhale.tistory.com/38
+    * */
 }
 
-fun main(){ //아직도 시간초과
+fun main() { //아직도 시간초과... O(n) 보다 작은 걸 써야한다...
     // 입력
     val br = BufferedReader(InputStreamReader(System.`in`))
     val sb = StringBuilder()
 
     val n = br.readLine().toInt()
-    val pointArr = Array(n){Point()}
     val st = StringTokenizer(br.readLine())
-    while(st.hasMoreTokens()){
-        val i = n-st.countTokens()
-        pointArr[i].apply {
-            originalIndex = i
-            originalValue = st.nextToken().toInt()
-        }
+    val points = Array(n){0}
+    while (st.hasMoreTokens()) {
+        val i = n - st.countTokens()
+        val v = st.nextToken().toInt()
+        points[i] = v
     }
-
-    pointArr.sortBy { it.originalValue }
-    var c = 0
-    var ci = 1
-
-    while(ci < n){ // O(n) = n 이 아닌가..?
-        if(pointArr[ci-1].originalValue != pointArr[ci].originalValue){
-            c++
-        }
-        pointArr[ci].compressIndex = ci
-        pointArr[ci].compressValue = c
-        ci++
-    }
-
-    pointArr.sortBy { it.originalIndex }
-
-    for(i in 0 until n){
-        sb.append(pointArr[i].compressValue).append(" ")
-    }
-    sb.deleteCharAt(sb.length-1)
-    print(sb)
 }
 
-data class Point(
-    var originalIndex : Int = 0,
-    var originalValue : Int = 0,
-    var compressIndex : Int = 0,
-    var compressValue : Int = 0
-)
-
 /*
-1. 정렬
-2. c=0, ci=0
-3. 맨 왼쪽에 c 값 넣기
-4. ci++
-
-LOOP
-5. pA[ci] 값이 pA[ci-1]과 같은가?
-5-1. 같음 : pA[ci] = c
-5-2. 다름 : pA[ci] = ++c
-6. ci++
-7. n <= ci 이면, 루프 끝
- */
+* 2 4 -10 4 -9
+* -10 -9 2 4 4
+*
+* map(v,i)
+* -10 0
+* -9 1
+* 2 2
+* 4 3
+*
+* 1. 입력 받을것 다 받고 그걸 원본 어레이에 저장
+* 2. 원본 어레이 하나 복제함, 그걸 value기준 sort.
+* 3. hashMap만듬
+* 4. 정렬한 어레이로 해쉬맵을 제작, key = v, value = 최초로 나오는 index
+* 5. 원본 어레이 0부터 끝까지 루프
+* 6. 원본 어레이 값을 가져와 키로 삼음
+* 7. 그 키로 해쉬맵 탐색
+* */
