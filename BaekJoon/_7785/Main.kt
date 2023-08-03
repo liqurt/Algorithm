612573f9766ef770a471b0ba8ba07e8cb7670a06
윤승일(Seung-Il Yoon) <liqurtJob@gmail.com>
2023-08-03 09:20:23 +0900

    BOJ7785 - treeMap 쓰기(메모리와 시간이 절약됨)

--- BaekJoon/_7785/Main.kt
+++ BaekJoon/_7785/Main.kt
@@ -2,6 +2,7 @@ package baekjoon._7785
 
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.util.TreeMap
 
 class Main {
 }
@@ -13,22 +14,22 @@ fun main(){
     val br = BufferedReader(InputStreamReader(System.`in`))
     val sb = StringBuilder()
 
-    val hm = HashMap<String, Boolean>()
+    val tm = TreeMap<String, Boolean>(reverseOrder())
     val n = br.readLine().toInt()
     for(i in 0 until n){
         val (name, state) = br.readLine().split(" ")
         if(state == enterMessage){
-            hm[name] = true
+            tm[name] = true
         }else if (state == leaveMessage){
-            if(hm.containsKey(name)){
-                hm.remove(name)
+            if(tm.containsKey(name)){
+                tm.remove(name)
             }
         }
     }
 
-    val nowWorking = hm.keys.toList().sortedDescending()
-    for(worker in nowWorking){
-        sb.append(worker).append("\n")
+    val tmIter = tm.iterator()
+    tmIter.forEach {
+        sb.append(it.key).append("\n")
     }
     sb.deleteCharAt(sb.lastIndex)
     print(sb)
