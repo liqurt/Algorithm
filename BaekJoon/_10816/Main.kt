30173440c9fa28284ea175b56028bb188dcad516
윤승일(Seung-Il Yoon) <liqurtJob@gmail.com>
2023-08-04 09:31:46 +0900

    BOJ10816 - lowerBound, upperBound 찾는식으로 해보자

--- BaekJoon/_10816/Main.kt
+++ BaekJoon/_10816/Main.kt
@@ -2,26 +2,50 @@ package baekjoon._10816
 
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.util.Arrays
 import java.util.StringTokenizer
 
 class Main {
 }
+
+// lower_Bound, upper_bound 찾는 식으로 해보자
 
 fun main(){
     val br = BufferedReader(InputStreamReader(System.`in`))
     val sb = StringBuilder()
 
     val n = br.readLine().toInt()
-    val numCards = Array<Int>(20000001){0}
-    val cards = StringTokenizer(br.readLine()," ")
+    val numArr = Array<Int>(n){0}
+    val st = StringTokenizer(br.readLine()," ")
     repeat(n){
-        numCards[cards.nextToken().toInt()+10000000]++
+        numArr[it] = st.nextToken().toInt()
     }
+    numArr.sort()
     val m = br.readLine().toInt()
-    val myCards = StringTokenizer(br.readLine()," ")
+    val st2 = StringTokenizer(br.readLine(), " ")
     repeat(m){
-        sb.append(numCards[myCards.nextToken().toInt()+10000000]).append(" ")
+        val target = st2.nextToken().toInt()
+        lower_bound(numArr, target)
+
     }
-    sb.deleteCharAt(sb.lastIndex)
-    print(sb)
+    println("")
+}
+
+// 정렬된 어레이를 받는다고 가정
+fun lower_bound(arr : Array<Int>, target : Int) : Int{
+    for(i in arr.indices){
+        if(arr[i] == target){
+            return i
+        }
+    }
+    return arr.size
+}
+
+fun upper_bound(arr : Array<Int>, target : Int) : Int{
+    for(i in arr.indices){
+        if(target < arr[i]){
+            return i
+        }
+    }
+    return arr.size
 }
