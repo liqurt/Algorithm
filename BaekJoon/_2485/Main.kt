437d6eb82dd0e83ca06ba5e89316160fa4ecd3fd
윤승일(Seung-Il Yoon) <liqurtJob@gmail.com>
2023-08-21 13:59:00 +0900

    BOJ2485 - 메모리 낭비하기 vs 수학

--- BaekJoon/_2485/Main.kt
+++ BaekJoon/_2485/Main.kt
@@ -2,84 +2,67 @@ package baekjoon._2485
 
 import java.io.BufferedReader
 import java.io.InputStreamReader
-import kotlin.properties.Delegates
 
 class Main {
-}
-
-const val MAX_NUM = 1000000000
-var n by Delegates.notNull<Int>()
-lateinit var trees: Array<Int>
-var minTree = MAX_NUM
-var maxTree = -1
-fun main() {
-
-    var minConsumedSapling = MAX_NUM
-    val br = BufferedReader(InputStreamReader(System.`in`))
-    val sb = StringBuilder()
-
-    n = br.readLine().toInt()
-    trees = Array<Int>(n){0}
-    repeat(n) {
-        val tree = br.readLine().toInt()
-        trees[it] = tree
-        if (tree < minTree) {
-            minTree = tree
-        }
-        if (maxTree < tree) {
-            maxTree = tree
-        }
-    }
-
-    val maxDistance = getMinTreeDistance()
-    for(interval in 1.. maxDistance){
-        val exampleStreet = plantTree(interval)
-        val consumedSapling = checkGoodInterval(exampleStreet)
-        if(0 < consumedSapling){
-            if(consumedSapling < minConsumedSapling){
-                minConsumedSapling = consumedSapling
-            }
-        }
-    }
-
-    sb.append(minConsumedSapling)
-    print(sb)
+    // -메모리를 오지게 먹는 방법-
     // 1. 주어진 가로수에서 최소 나무 거리(T)를 구한다.
     // 2. 1부터 최소나무거리(포함) 까지 나무를 심어본다.
     // 3. 나무를 심은 가로수의 간격이 일정한지 확인한다.
     // 4. 일정하다면, 그것을 위해 소비한 나무의 수를 센다.
     // 5. 일정하지 않다면, 무시한다.
     // 6. 소비한 나무의 수 중, 가장 적게 소비한 나무를 출력.
+
+    // -메모리를 오지게 안 먹는 방법-
+    // 1. 주어진 나무들 간의 간격을 구한다.
+    // 2. 간격들의 최대공약수가 곧 최적의 간격이 된다.
+    // 3. 심은 나무를 구한다.
 }
 
-fun checkGoodInterval(exampleStreet: List<Int>): Int {
-    val firstInterval = exampleStreet[1] - exampleStreet[0]
-    for (location in 1 until exampleStreet.size - 1) {
-        val interval = exampleStreet[location + 1] - exampleStreet[location]
-        if (interval != firstInterval) {
-            return -1
-        }
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val sb = StringBuilder()
+
+    val n = br.readLine().toInt()
+    val street = mutableListOf<Int>()
+    repeat(n){
+        street.add(br.readLine().toInt())
     }
-    return exampleStreet.size - n
+
+    val intervals = treeInterval(street)
+    val oI = optimalInterval(intervals)
+    val result = plantingTrees(street, oI, n)
+    sb.append(result)
+    print(result)
 }
 
-fun plantTree(interval: Int): List<Int> {
-    var plantedStreet = ArrayList<Int>()
-    plantedStreet.addAll(trees)
-    for(location in minTree .. maxTree step(interval)){
-        plantedStreet.add(location)
+fun optimalInterval(intervals : List<Int>): Int {
+    var interval = gcd(intervals[0],intervals[1])
+    for(i in 2 .. intervals.lastIndex){
+        interval = gcd(interval, intervals[i])
     }
-    val a = plantedStreet.distinct().sorted()
-    return a
+    return interval
 }
 
-fun getMinTreeDistance(): Int{
-    var minDistance = MAX_NUM
-    for(i in 0 until trees.size-1){
-        val distance = trees[i+1] - trees[i]
-        if(distance < minDistance){
-            minDistance = distance
-        }
+fun plantingTrees(street: List<Int>, optimalInterval: Int, givenTrees: Int): Int {
+    var count = 0
+    for (location in street[0]..street[street.lastIndex] step (optimalInterval)) {
+        count++
     }
-    return minDistance
+    return count - givenTrees
+}
+
+fun treeInterval(street : List<Int>) : List<Int>{
+    val intervals = mutableListOf<Int>()
+    repeat(street.size - 1){
+        intervals.add(street[it+1] - street[it])
+    }
+    return intervals
+}
+
+fun gcd(a : Int, b : Int) : Int{
+    return if(b == 0) {
+        a
+    }else{
+        gcd(b, a%b)
+    }
 }
