8931ebfcbcd4bd4102407d23d8226f191c399ec0
윤승일(Seung-Il Yoon) <liqurtJob@gmail.com>
2023-09-08 14:14:10 +0900

    BOJ11866 - 정석적인 큐 풀이

--- BaekJoon/_11866/Main.kt
+++ BaekJoon/_11866/Main.kt
@@ -2,43 +2,37 @@ package baekjoon._11866
 
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.util.LinkedList
 
 class Main {
 }
 
-fun main(){
+fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val sb = StringBuilder()
     val (n, k) = br.readLine().split(" ").map { it.toInt() }
-    val inputData = ArrayList<Int>()
-    repeat(n){
-        inputData.add(it+1)
+    val inputData = LinkedList<Int>()
+    repeat(n) {
+        inputData.add(it + 1)
     }
-    sb.append('<').append(josephus(inputData,n, k)).deleteCharAt(1).deleteCharAt(sb.lastIndex).append('>')
+    sb.append('<').append(josephus(inputData, n, k)).deleteCharAt(1).deleteCharAt(sb.lastIndex).append('>')
     print(sb)
 }
 
-fun josephus(q: ArrayList<Int>, n : Int, k : Int): ArrayList<Int> {
+//    놀랍게도, 브루트포스말고 큐로도 해결볼수 있다.
+//    1. 'k-1' 회 까지는 큐 앞에서 뽑아 뒤로 붙인다.
+//    2. 'k' 회 차의 아이템은 요세푸스 순열에 놓는다.
+//    근데, 브루트포스로 하는게 메모리를 덜 쓰네? 시간은 비슷함
+fun josephus(q: LinkedList<Int>, n: Int, k: Int): ArrayList<Int> {
     val result = ArrayList<Int>()
-    val counted = ArrayList<Boolean>()
-    repeat(n){
-        counted.add(false)
-    }
-
-    var falseCount = 0
-    var index = 0
+    var step = 1
     while(result.size < n){
-        if(!counted[index]){
-            falseCount++
-            if(falseCount == k){
-                falseCount = 0
-                result.add(q[index])
-                counted[index] = true
-            }
-        }
-        index++
-        if(n <= index){
-            index = 0
+        if(step < k){
+            q.addLast(q.pollFirst())
+            step++
+        }else{
+            result.add(q.pollFirst())
+            step = 1
         }
     }
     return result
