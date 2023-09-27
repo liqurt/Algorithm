726fb9434b64542bfb73ba75c82c6678148744b3
윤승일(Seung-Il Yoon) <liqurtJob@gmail.com>
2023-09-27 15:34:48 +0900

    BOJ15439

---
+++ BaekJoon/_15439/Main.kt
@@ -0,0 +1,16 @@
+package baekjoon._15439
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+class Main {
+}
+
+fun main(){
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val sb = StringBuilder()
+
+    val n = br.readLine().toInt()
+    val result = n*n-n
+    print(result)
+}
